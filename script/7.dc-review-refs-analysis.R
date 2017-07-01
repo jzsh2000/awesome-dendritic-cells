ad5a30ee6e01116b6f02268562c33cb0a5ba80b8
Xiaoyang Jin <jinxiaoyang@jinxiaoyang.cn>
2017-07-02 00:16:26 +0800

    Combine linked cliques

--- script/7.dc-review-refs-analysis.R
+++ script/7.dc-review-refs-analysis.R
@@ -24,7 +24,29 @@ pubmed_cooccur_graph = graph_from_edgelist(
                   select(-n)), directed = FALSE)
 
 pubmed_cooccur_cliques = max_cliques(pubmed_cooccur_graph, min = 3)
-write_lines(map_chr(pubmed_cooccur_cliques,
-                    ~paste(names(.x)[order(as.numeric(names(.x)))],
-                           collapse = '-')),
-            path = 'output/dc-review-refs.cliques.txt')
+
+# combine cliques
+pubmed_cooccur_cliques_matrix = sapply(pubmed_cooccur_cliques, function(x) {
+    sapply(pubmed_cooccur_cliques, function(y) {
+        length(intersect(names(x), names(y)))
+        })
+    }) >= 2
+pubmed_cooccur_cliques_graph = graph_from_adjacency_matrix(
+    pubmed_cooccur_cliques_matrix,
+    mode = 'undirected'
+)
+pubmed_cooccur_cliques_clusters = clusters(pubmed_cooccur_cliques_graph)
+pubmed_cooccur_cliques_clusters = groups(pubmed_cooccur_cliques_clusters)
+
+pubmed_cooccur_cliques_new = sapply(pubmed_cooccur_cliques_clusters,
+                                    function(x) {
+                                        Reduce(union,
+                                               lapply(pubmed_cooccur_cliques[x],
+                                                      names))
+                                    })
+sapply(pubmed_cooccur_cliques_new,
+       function(x) {
+           paste(x[order(as.numeric(x))],
+                 collapse = ' ')
+       }) %>%
+    write_lines(path = 'output/dc-review-refs.cliques.txt')
